be114c97c17f843a0a4d4d04ccfcd28a0e096c04
IgorDzeverin <71033728+IgorDzeverin@users.noreply.github.com>
2021-05-07 09:26:43 +0300

    Update lvpv.r

--- lvpv.r
+++ lvpv.r
@@ -2,7 +2,7 @@
 
 # an implementation of a multiscale bootstrap procedure designed to estimate the p-values in the modular structure analysis with the leading eigenvector algorithm
 
-# by Oksana Vertsimakha and Igor Dzeverin, 2020-09-10
+# by Oksana Vertsimakha and Igor Dzeverin, 2021-04-27
 
 # References
 # Csardi, G., Nepusz, T. (2006). The igraph software package for complex network research. InterJournal, Complex Systems 1695. http://igraph.org
